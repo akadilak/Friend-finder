e90591dea47232907c7f19e335558ddc116b0f07
unknown <amkadilak@gmail.com>
2018-01-01 17:13:15 -0500

    just have to deploy to heroku

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Gerard Butler',
+		photo: 'https://pbs.twimg.com/media/CwA5ar0WYAAbhUz.jpg',
+		scores: [
+			'3',
+			'5',
+			'5',
+			'2',
+			'3',
+			'4',
+			'4',
+			'1',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Selena Gomez',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg/492px-Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg',
+		scores: [
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Chris Hemsworth',
+		photo: 'https://resizing.flixster.com/zWfpiT1Yj-OaXcj27XMJd-W2a2g=/300x300/v1.cjs0MjUwMjtqOzE3NTUwOzEyMDA7MjUxOTszMDAw',
+		scores: [
+			'4',
+			'2',
+			'5',
+			'2',
+			'1',
+			'3',
+			'5',
+			'3',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Parker McKenna Posey',
+		photo: 'https://i.pinimg.com/564x/57/2c/59/572c59d8b54e69b7aedef9d10492ebd9--parker-posey-child.jpg',
+		scores: [
+			'4',
+			'1',
+			'2',
+			'5',
+			'3',
+			'4',
+			'5',
+			'3',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Ryan Reynolds',
+		photo: 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2Fimages%2F2015%2F11%2F111215-ryan-reynolds-2.jpg%3Fitok%3DrWpFcN-k&w=700&q=85',
+		scores: [
+			'2',
+			'1',
+			'5',
+			'4',
+			'3',
+			'5',
+			'2',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Taraji P. Henson',
+		photo: 'https://vignette4.wikia.nocookie.net/empire/images/0/02/Taraji_P_Henson.jpg/revision/latest?cb=20150412121731&path-prefix=it',
+		scores: [
+			'2',
+			'1',
+			'2',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Dwayne Johnson',
+		photo: 'https://media.tmz.com/2017/09/19/the-rock-dwayne-johnson-profile-200x250.jpg',
+		scores: [
+			'5',
+			'3',
+			'4',
+			'2',
+			'3',
+			'4',
+			'3',
+			'1',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Gabrielle Union',
+		photo: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwNjUzMDM2Nl5BMl5BanBnXkFtZTcwNDgyMDY4Mg@@._V1_UY317_CR27,0,214,317_AL_.jpg',
+		scores: [
+			'3',
+			'2',
+			'4',
+			'1',
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Julian Edelman',
+		photo: 'https://menshairstylesclub.com/wp-content/uploads/2017/06/Julian-Edelman.jpg',
+		scores: [
+			'2',
+			'5',
+			'3',
+			'5',
+			'4',
+			'5',
+			'1',
+			'5',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Jennifer Lopez',
+		photo: 'https://celluloidjunkie.com/wp-content/uploads/2017/06/Jennifer-Lopez-Headshot-by-Gabriel-Goldman-e1498168073502.jpg',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
